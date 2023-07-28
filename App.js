9689c55a8f9f58d654393cc62394011463fddfe6
Shreya <shreyasharma.is18@bmsce.ac.in>
2023-07-28 22:19:58 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading= React.createElement("div",{
+    id:'heading',xyz:'abc'
+},React.createElement("div", {id:'child'},[React.createElement("h1",{},'I am Child 1'),
+React.createElement("h2",{},'I am Child 2')]));
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
