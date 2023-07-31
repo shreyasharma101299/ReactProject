633a9f6c9558e45ae223ffdfff95b8fdf1530755
Shreya <shreyasharma.is18@bmsce.ac.in>
2023-07-31 18:54:43 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading= React.createElement("div",{
-    id:'heading',xyz:'abc'
-},React.createElement("div", {id:'child'},[React.createElement("h1",{},'I am Child 1'),
-React.createElement("h2",{},'I am Child 2')]));
+const heading = React.createElement(
+  "div",
+  {
+    id: "heading",
+    xyz: "abc",
+  },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am Child 1"),
+    React.createElement("h2", {}, "I am Child 2"),
+  ])
+);
+const Title = () => <h1>Title of Functional Component</h1>;
+const FunctionalComponent = () => (
+  <div>
+    <Title />
+    <h1>Welcome to functional component</h1>
+  </div>
+);
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const jsxElement = <h1 className="heading">Welcome Hello World</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<FunctionalComponent />);
